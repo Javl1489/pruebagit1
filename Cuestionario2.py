00a5913bf6564eac2d1fcb694653b7c03f5b4288
javl1489 <154706475+Javl1489@users.noreply.github.com>
2024-01-09 16:14:59 +0000

    nuevo cuestionario

---
+++ Cuestionario2.py
@@ -0,0 +1,16 @@
+Nombre = input("cual es tu nombre: ") 
+Apellido = input("cual es tu apellido: ")
+contraseña_str = input("cual es tu contraseña: ")
+
+contraseña_str = int(contraseña_str)
+
+if contraseña_str == 1489:
+    print("Hola johnny")
+
+    input("actualiar datos:")
+    direccion = input("direccion: ")
+    ciudad = input("ciudad: ")
+    print(direccion, ciudad)
+
+else:
+    print("no eres Johny")
